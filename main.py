f68f93eb3011c023b672bae07bf33f3572c4fa2b
David Drobner <david@drobner.ca>
2019-10-25 17:19:30 -0400

    Forgot to commit

--- main.py
+++ main.py
@@ -1,6 +1,31 @@
 from spotify import spotify
+from config import config
+
+import time
+from watchdog.observers import Observer
+from watchdog.events import PatternMatchingEventHandler
 
 spotify = spotify()
+config = config()
 
-track_id = spotify.search("Fall for U")
-spotify.download_art(track_id)
+
+
+eventHandler = PatternMatchingEventHandler("currentsong.txt", "", "", False)
+
+def onModified(event):
+    f = open(f"{config.getDirectory()}\\config.txt")
+
+    track_id = spotify.search("Stardust lucas and steve")
+    spotify.download_art(track_id)
+
+eventHandler.on_modified = onModified
+
+fileObserver = Observer()
+fileObserver.schedule(eventHandler, config.getDirectory(), False)
+fileObserver.start()
+
+try:
+    time.sleep(1)
+except KeyboardInterrupt:
+    fileObserver.stop()
+    fileObserver.join()
