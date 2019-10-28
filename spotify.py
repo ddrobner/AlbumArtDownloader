852b7def21cf333bdb509afe4c5beea6ed01001d
David Drobner <david@drobner.ca>
2019-10-28 14:00:07 -0400

    Used a try and except block to reauthenticate if the requests error

--- spotify.py
+++ spotify.py
@@ -11,13 +11,21 @@ class spotify():
         self.access_token = self.authenticator.authenticate()
 
     def search(self, title):
-        self.result = json.loads(requests.get(f"https://api.spotify.com/v1/search?q={title}&type=track", headers={'Authorization' : f"Bearer {self.access_token}"}).content)
+        try:
+            self.result = json.loads(requests.get(f"https://api.spotify.com/v1/search?q={title}&type=track", headers={'Authorization' : f"Bearer {self.access_token}"}).content)
+        except:
+            print("Invalid token... Attemping authentication again")
+            self.access_token = self.authenticator.authenticate()
         return  self.result['tracks']['items'][0]['id']
 
     def download_art(self, album_id):
-        self.album = json.loads(requests.get(f"https://api.spotify.com/v1/tracks/{album_id}", headers={'Authorization': f"Bearer {self.access_token}"}).content)
-        self.image_url =  self.album['album']['images'][0]['url']
-        image = requests.get(self.image_url, stream=True)
+        try:
+            self.album = json.loads(requests.get(f"https://api.spotify.com/v1/tracks/{album_id}", headers={'Authorization': f"Bearer {self.access_token}"}).content)
+            self.image_url =  self.album['album']['images'][0]['url']
+            image = requests.get(self.image_url, stream=True)
+        except:
+            print("Invalid token... Attemping authentication again")
+            self.access_token = self.authenticator.authenticate()
 
         with open('album.jpg', 'wb') as out_file:
             shutil.copyfileobj(image.raw, out_file)
