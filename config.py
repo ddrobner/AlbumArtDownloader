d4502e4369b6c3b80db83778bafc66bf02165376
David Drobner <david@drobner.ca>
2019-11-28 23:06:03 -0500

    Prepared repo for publication

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ import base64
 
 class config():
     def __init__(self):
-        self.client_id = "c4a5b878e2b74cf885b2843a14281dbc"
-        self.client_secret = "b16c80dc53c3419487da71e1be4fa2ca"
+        self.client_id = "CLIENT_ID_HERE"
+        self.client_secret = "CLIENT_SECRET_HERE"
         self.token = base64.b64encode(str.encode(f"{self.client_id}:{self.client_secret}", 'utf-8')).decode('utf-8')
 
     def getToken(self):
