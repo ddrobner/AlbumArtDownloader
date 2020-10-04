3a31b8e5e6ed37068d32533cdb24a9f8c8a89944
Shumatsu Mokuyobi <shumatsum@protonmail.ch>
2020-10-04 18:45:39 +0200

    Clean up
    Cleaned up the code to adhere to pip8 and the .editorconfig

--- spotify.py
+++ spotify.py
@@ -5,27 +5,30 @@ import shutil
 from auth import auth
 from config import config
 
+
 class spotify():
     def __init__(self):
         self.config = config()
-        
-        self.AUTHENTICATION_STRING = self.config.getToken() 
+
+        self.AUTHENTICATION_STRING = self.config.getToken()
         self.authenticator = auth(self.AUTHENTICATION_STRING)
         self.access_token = self.authenticator.authenticate()
 
     def search(self, title):
         try:
-            self.result = json.loads(requests.get(f"https://api.spotify.com/v1/search?q={title}&type=track", headers={'Authorization' : f"Bearer {self.access_token}"}).content)
+            self.result = json.loads(requests.get(f"https://api.spotify.com/v1/search?q={title}&type=track", headers={
+                                     'Authorization': f"Bearer {self.access_token}"}).content)
         except:
             # Tokens expire after an hour so it reauthenticates if the request fails (done to avoid a mess keeping track of tokens)
             print("Invalid token... Attemping authentication again")
             self.access_token = self.authenticator.authenticate()
-        return  self.result['tracks']['items'][0]['id']
+        return self.result['tracks']['items'][0]['id']
 
     def download_art(self, album_id):
         try:
-            self.album = json.loads(requests.get(f"https://api.spotify.com/v1/tracks/{album_id}", headers={'Authorization': f"Bearer {self.access_token}"}).content)
-            self.image_url =  self.album['album']['images'][0]['url']
+            self.album = json.loads(requests.get(f"https://api.spotify.com/v1/tracks/{album_id}", headers={
+                                    'Authorization': f"Bearer {self.access_token}"}).content)
+            self.image_url = self.album['album']['images'][0]['url']
             image = requests.get(self.image_url, stream=True)
         except:
             # Tokens expire after an hour so it reauthenticates if the request fails (done to avoid a mess keeping track of tokens)
