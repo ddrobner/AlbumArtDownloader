fa06a459fb329838b212bee7f5899ed5952f211d
David Drobner <david@drobner.ca>
2020-10-04 13:18:27 -0400

    Using environment variables for auth

--- config.py
+++ config.py
@@ -1,9 +1,10 @@
 import base64
+import os
 
 class config():
     def __init__(self):
-        self.client_id = "CLIENT_ID_HERE"
-        self.client_secret = "CLIENT_SECRET_HERE"
+        self.client_id = os.environ.get("SPOTIFY_CLIENT_ID")
+        self.client_secret = os.environ.get("SPOTIFY_CLIENT_SECRET")
         self.token = base64.b64encode(str.encode(f"{self.client_id}:{self.client_secret}", 'utf-8')).decode('utf-8')
 
     def getToken(self):
